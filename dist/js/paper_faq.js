672351bbf07fd13d57892a719c48cfca26de343a
Martina Kurkova <martinanoha@seznam.cz>
2026-03-06 17:12:07 +0100

    odstoupeni od smlouvy

--- dist/js/paper_faq.js
+++
@@ -1,8 +0,0 @@
-// paper-faq
-const togglesPaper = document.querySelectorAll('.paper-faq__toggle')
-
-togglesPaper.forEach(toggle => {
-    toggle.addEventListener('click', () => {
-        toggle.parentNode.classList.toggle('active')
-    })
-})
